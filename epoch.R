9564cbd72413d553a5ce6b5fa3513bb291b22ac5
Rahulrdy52 <rahulmreddy5@gmail.com>
2025-01-29 10:22:29 -0500

    mistake

--- epoch.R
+++ epoch.R
@@ -5,18 +5,12 @@ run_epoch <- function(down, ytg, fp) {
   
 
   while (score == 0 && drives < 10) {
-    state <- run_drive(down, ytg, fp) 
-    # Simulate a drive and return the updated game state
+    state <- run_drive(down, ytg, fp)
     score <- process_drive_result(state)
-     # Check if a score occurred from the updated state
-     
-      # # If a score occurs, adjust it based on which team scored 
-     #(+ for reference, - for opponent)
     if (score != 0) score <- team * score 
-    team <- -team # Switch possession: If the reference team had the ball, 
-    # give it to the opposite and vice versa
-    drives <- drives + 1 # Increment drive count to prevent infinite loops
+    team <- -team
+    drives <- drives + 1
   }
   
-  score # The last evaluated expression is implicitly returned
+  return(score)
 }
