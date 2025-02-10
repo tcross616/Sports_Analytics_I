8a75894c36a0cc8c87e57fe705b087084871e5d6
Rahulrdy52 <141646866+Rahulrdy52@users.noreply.github.com>
2025-02-10 10:40:52 -0500

    Delete drive.R

--- drive.R
+++
@@ -1,9 +0,0 @@
-process_drive_result <- function(fp) {
-  if (fp > 80) return(7)
-  if (fp > 40) return(3) 
-  if(fp < 20) return(-3)
-  if (fp < 0) return(-7)   
-  return(0)                
-}
-process_drive_result(90)
-expected_points <- simulate_epoch(down = 1, ytg = 10, fp =70)
