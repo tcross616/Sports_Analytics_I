123fde255128851142e8fa9596128bd8f2cdca24
Rahulrdy52 <rahulmreddy5@gmail.com>
2025-02-26 10:50:36 -0500

    setup

---
+++ setup.R
@@ -0,0 +1,17 @@
+# 1. Load models
+source("logistic_regression.R")
+source("multimodal_reg.R")
+
+# 2. Load low-level simulation code
+source("down_one.R")
+source("down_two.R")
+source("down_three.R")
+source("down_four.R")
+source("run_play.R")
+source("rundrive.R")
+source("epoch.R")
+
+# 3. Load top-level user-facing functions
+source("expectedpoints.R")
+
+# 4. (Optionally) run tests or demos
