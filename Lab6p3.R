18cd7a4df745ccec6f90264d219a8d4a693a2927
Teddy Cross <fva3zb@virginia.edu>
2025-02-28 10:46:02 -0500

    Update Lab6p3.R

--- Lab6p3.R
+++ Lab6p3.R
@@ -1,1 +1,11 @@
+# Specify a coach name to filter the data
+selected_coach <- "GUY BOUCHER"
+coach_shots_df <- shots_df %>% filter(Home_Coach == selected_coach | Away_Coach == selected_coach)
+
+# Visualizing Shot Success Probability using Gaussian Distribution
+ggplot(coach_shots_df, aes(x = xC, y = yC)) +
+  stat_density2d(aes(fill = ..level..), geom = "polygon") +
+  scale_fill_gradient(low = "blue", high = "red") +
+  theme_minimal() +
+  labs(title = paste("Shot Success Probability for", selected_coach), x = "X Coordinate", y = "Y Coordinate")
 
