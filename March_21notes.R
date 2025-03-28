638c50f76ebbbf1d51b048b8e63b7b9a929d8f5b
Teddy Cross <fva3zb@virginia.edu>
2025-03-28 09:53:51 -0400

    Update March_21notes.R

--- March_21notes.R
+++ March_21notes.R
@@ -1,1 +1,16 @@
+# Install necessary packages if not already installed
+install.packages(c("nflfastR", "dplyr", "mixtools","em
+"))
+seasons <- 2020
+pbp_data <- load_pbp(seasons)
+glimpse(pbp_data)
+play_type_probs <- pbp_data %>%
+  filter(play_type %in% c("run", "pass")) %>%
+  count(play_type) %>%
+  mutate(prob = n / sum(n))
+incompletion_prob <- pbp_data %>%
+  filter(play_type == "pass") %>%
+  summarise(prob = mean(!complete_pass, na.rm = TRUE))
+fumble_prob <- pbp_data %>%
+  summarise(prob = mean(fumble == 1, na.rm = TRUE))
 
